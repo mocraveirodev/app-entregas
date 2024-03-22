da4ab3c6bbd2db2a7a454c1b75e3bb229c5afbce
Monica Craveiro <craveiromonica@gmail.com>
2024-03-21 23:45:39 -0300

    Restarting project

--- scripts/database.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists users (
-    id integer auto_increment primary key,
-    login varchar(100) unique not null,
-    password varchar(100) not null,
-    type enum('admin', 'entregador', 'empresa') not null
-)
--- src/server.js
+++
@@ -1,5 +0,0 @@
-import app from './app.js';
-
-app.listen(3000, () => {
-    console.info('Server running on http://localhost:3000');
-});
